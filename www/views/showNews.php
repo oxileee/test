36e4072fc67080e8dc9667ff30a03f576bfa8c6a
Vitaly Kozachkov <vitaly.kozachkov1993@gmail.com>
2020-05-09 23:31:02 +0300

    Реализовал вывод списка новостей на экран

--- www/views/showNews.php
+++ www/views/showNews.php
@@ -1,3 +1,26 @@
 <?php
-
+    require __DIR__ . '/../models/sql.php';
+    $link = new Database('localhost', 'root', '', 'test');
+    $link->connect();
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Show News</title>
+</head>
+<body>
+    <h1>Новости</h1>
+    <a href="addNews.php">Добавить новость</a>
+    <hr>
+<?php
+    $res = $link->getNews();
+    if(mysqli_num_rows($res) > 0) { // проверка на количество строк в БД
+        while ($row = mysqli_fetch_array($res)) { // цикл обращения к элементам БД
+            echo 'Название: ' . $row['title'] . '<br />Текст: ' . $row['text'] . '<br />Автор: ' . $row['author'] . '<hr />';
+        }
+    }
+?>
+
+</body>
+</html>
