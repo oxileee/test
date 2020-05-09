125f041c781b095a7df822a5de4fe4ba8ecec77c
Vitaly Kozachkov <vitaly.kozachkov1993@gmail.com>
2020-05-09 20:30:14 +0300

    Добавил в контроллер index.php содержимое

---
+++ www/index.php
@@ -0,0 +1,19 @@
+<?php
+    require_once __DIR__ . '/models/sql.php';
+
+    if(!$_POST) {
+        header ('Location: /views/addNews.php');
+        exit;
+    }
+
+    $link = new Database('localhost', 'root', '', 'test');
+    $link->connect();
+
+    $title = $_POST['title'];
+    $text = $_POST['text'];
+    $author = $_POST['author'];
+
+    $link->addNews($title, $text, $author);
+
+    header ('Location: /views/addNews.php');
+?>
